587d0aa46bcb6d03cb19bb7a5ad805cc03c6e977
broccoli88 <kode.kudaki@gmail.com>
2022-03-12 07:21:53 +0100

    fully functional simple calculator - finished

--- js/script.js
+++ js/script.js
@@ -7,69 +7,84 @@ const clear = document.querySelector("[data-clear]");
 const calculator = document.querySelector(".calculator");
 const del = document.querySelector("[data-delete]");
 
-// add number to screen
-// add operation
-// clear
-// COMPUTING
+function calculatorFunctionality() {
+  // ADDING NUMBER TO SCREEN
 
-// ADDING NUMBER TO SCREEN
+  numbers.forEach((number) => {
+    number.addEventListener("click", (e) => {
+      const numberValue = e.target.textContent;
+      const displayedNumber = display.textContent;
 
-numbers.forEach((number) => {
-  number.addEventListener("click", (e) => {
-    const numberValue = e.target.textContent;
-    const displayedNumber = display.textContent;
+      if (displayedNumber === "0") {
+        display.textContent = numberValue;
+      } else {
+        display.textContent += numberValue;
+      }
 
-    if (displayedNumber === "0") {
-      display.textContent = numberValue;
-    } else {
-      display.textContent += numberValue;
+      calculator.dataset.operation = "inactive";
+    });
+  });
+
+  // ADDING DECIMAL
+
+  decimal.addEventListener("click", () => {
+    if (display.textContent.includes(".")) return;
+    else {
+      display.textContent += decimal.textContent;
     }
+  });
 
+  //  CLEARING DISPLAY
+
+  clear.addEventListener("click", () => {
+    display.textContent = "0";
     calculator.dataset.operation = "inactive";
   });
-});
 
-// ADDING DECIMAL
+  // DELETING LAST ITEM
 
-decimal.addEventListener("click", () => {
-  if (display.textContent.includes(".")) return;
-  else {
-    display.textContent += decimal.textContent;
-  }
-});
+  del.addEventListener("click", () => {
+    if (
+      display.textContent.charAt(display.textContent.length - 1) === "+" ||
+      display.textContent.charAt(display.textContent.length - 1) === "-" ||
+      display.textContent.charAt(display.textContent.length - 1) === "*" ||
+      display.textContent.charAt(display.textContent.length - 1) === "/"
+    ) {
+      calculator.dataset.operation = "inactive";
+    }
 
-//  CLEARING DISPLAY
-
-clear.addEventListener("click", () => {
-  display.textContent = "0";
-  calculator.dataset.operation = "inactive";
-});
-
-// DELETING LAST ITEM
-
-// del.addEventListener("click", () => {
-//   if (display.textContent.charAt(-1) === "+") {
-//     console.log(true);
-//   }
-// });
-
-// ADDING OPERATION
-
-operations.forEach((operation) => {
-  operation.addEventListener("click", (e) => {
-    const operationValue = e.target.textContent;
-    if (calculator.dataset.operation === "active") {
-      return;
-    } else {
-      display.textContent += operationValue;
-      calculator.dataset.operation = "active";
+    display.textContent = display.textContent.slice(0, -1);
+    if (!display.textContent) {
+      display.textContent = "0";
     }
   });
-});
 
-// CALCULATION
+  // ADDING OPERATION
 
-equals.addEventListener("click", () => {
-  display.textContent = eval(display.textContent);
-  calculator.dataset.operation = "inactive";
-});
+  operations.forEach((operation) => {
+    operation.addEventListener("click", (e) => {
+      const operationValue = e.target.textContent;
+      if (
+        (calculator.dataset.operation === "active" &&
+          display.textContent.charAt(display.textContent.length - 1) === "+") ||
+        display.textContent.charAt(display.textContent.length - 1) === "-" ||
+        display.textContent.charAt(display.textContent.length - 1) === "*" ||
+        display.textContent.charAt(display.textContent.length - 1) === "/"
+      ) {
+        return;
+      } else {
+        display.textContent += operationValue;
+        calculator.dataset.operation = "active";
+      }
+    });
+  });
+
+  // CALCULATION
+
+  equals.addEventListener("click", () => {
+    display.textContent = eval(display.textContent);
+    calculator.dataset.operation = "inactive";
+  });
+}
+
+calculatorFunctionality();
